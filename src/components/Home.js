d6bb783a16d4fbcd853b4dae2b875238b6aa865f
Justin Connolly <justin.connolly@gmail.com>
2022-08-23 12:28:43 -0400

    Updated home message to include link to graph repo.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ function Home() {
   return (
     <div className="App">
       <Container>
-        Hi, I'm Justin. I'm a fourth year student in the Bachelor of Computer Science program at Carleton University in Ottawa, Ontario.
-        I'm currently in the Accelerated MCS prepatory program, allowing me to take graduate courses this upcoming year.
+        Hi, I'm Justin. I'm a fourth year student in the Bachelor of Computer Science Honours program at Carleton University in Ottawa, Ontario. I'm also in the Accelerated MCS prepatory program, allowing me to take graduate courses this upcoming year.<br/>
         I like algorithm design, graphs, combinatorics, and pretty much anything that can be a puzzle.
+        In my spare time I've been working on a graph module in python to learn associated algorithms and related data structures, check it out <a href="https://github.com/justinconnolly/graph-algorithms">here</a>.
       </Container>
     </div>
   );
