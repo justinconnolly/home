78417f0456b059debd21b6577fc0412a4183c344
Justin Connolly <justin.connolly@gmail.com>
2024-03-19 20:58:20 -0400

    Updated about

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ function Home() {
   return (
     <div className="App">
       <Container>
-        Hi, I'm Justin. I'm a computer scientist from Ottawa, Ontario.
-        I like algorithm design, graphs, combinatorics, and pretty much anything that can be a puzzle.
-        I put together a graph module in python to study some algorithms and related data structures, check it out <a href="https://github.com/justinconnolly/graph-algorithms">here</a>.
+        Hi, I'm Justin. I'm a computer scientist and software engineer from Ottawa, Ontario.
+        I like algorithm design, graphs, combinatorics, and pretty much anything that can be a puzzle.<br/>
+        I put together a graph module in python to study some algorithms and related data structures, check it out <a href="https://github.com/justinconnolly/graph-algorithms">here</a>.<br/>
         Check out my python library for the Garmin LiDAR Lite v3 <a href="https://github.com/justinconnolly/Lidar-Lite-V3/">here</a>.
       </Container>
     </div>
