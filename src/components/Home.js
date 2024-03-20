52227c0ac461019929008956126b0d92fb8347cd
Justin Connolly <justin.connolly@gmail.com>
2024-03-19 20:18:53 -0400

    Updated about section.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,10 @@ function Home() {
   return (
     <div className="App">
       <Container>
-        Hi, I'm Justin. I'm a fourth year student in the Bachelor of Computer Science Honours program at Carleton University in Ottawa, Ontario. I'm also in the Accelerated MCS prepatory program, allowing me to take graduate courses this upcoming year.<br/>
+        Hi, I'm Justin. I'm a computer scientist from Ottawa, Ontario. I 
         I like algorithm design, graphs, combinatorics, and pretty much anything that can be a puzzle.
         In my spare time I've been working on a graph module in python to learn associated algorithms and related data structures, check it out <a href="https://github.com/justinconnolly/graph-algorithms">here</a>.
+        Check out my python library for the Garmin LiDAR Lite v3 <a href="https://github.com/justinconnolly/Lidar-Lite-V3/">here</a>.
       </Container>
     </div>
   );
